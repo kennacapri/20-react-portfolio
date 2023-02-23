742a0139741d61d65402d6519177382ad27efa2d
Kenna Fackrell <kennafackrell@fackrell-imac.lan>
2023-02-23 13:16:35 -0700

    fixed stying with footer and updated about me paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,30 @@
-import React from "react"
+import React from "react";
 
 const About = () => {
   return (
     <div name="about" className="w-full h-screen bg-[#DBC4A9] text-[#253031]">
       <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-          <div className='sm:text-right pb-8 pl-4'>
-            <p className='text-4xl font-bold inline border-b-4 border-[#0a8754]'>About</p>
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-[#0a8754]">
+              About
+            </p>
           </div>
           <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold">
+            <p> Hi. I'm Kenna. Welcome to my portfolio!</p>
           </div>
-          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-            <div className='sm:text-right text-4xl font-bold'>
-                <p> Hi. I'm Kenna. Welcome to my portfolio!</p>
-            </div>
-            <div>
-                <p> I'm passionate about building excellent software that improves the lives of those around me. I specialize in creating software for clients ranging from individuals and small-businesses. What would you do if you had a software expert available at your fingertips?</p>
+          <div>
+            <p>
+              {" "}
+              My passion lies in developing top-notch software that enhances the
+              quality of life for those in my community. With expertise in
+              crafting software for a diverse range of clients, including
+              individuals and small businesses, imagine having a software expert
+              at your beck and call. What possibilities would open up for you?
+            </p>
           </div>
         </div>
       </div>
@@ -23,4 +32,4 @@ const About = () => {
   );
 };
 
-export default About
+export default About;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -152,7 +152,7 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-      <footer class="fixed bottom-0 left-0 z-20 w-full p-2 bg-[#DBC4A9] border-gray-200 shadow md:flex md:items-center md:justify-between md:p-3 dark:bg-gray-800 dark:border-gray-600">
+      <footer class="fixed bottom-0 left-0 z-20 w-full p-2 bg-[#DBC4A9] border-gray-200 shadow md:flex md:items-center md:justify-between md:p-3">
         <span class="text-sm text-black sm:text-center dark:black">
           © 2023{" "}
           <a href="https://github.com/kennacapri" class="hover:underline">
